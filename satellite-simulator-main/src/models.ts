fdd1c48b3796770a851961ec4dc057f54bbffb24
yasmin Ebrahim <yasmineebrahim458@gmail.com>
2025-07-09 16:17:44 +0300

    ...

--- satellite-simulator-main/src/models.ts
+++
@@ -1,6 +0,0 @@
-import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
-
-import satellite from 'assets/satellite/Satellite.glb';
-
-const gltfLoader = new GLTFLoader();
-export const satelliteModel = gltfLoader.loadAsync(satellite);
--- satellite-simulator-main/src/physics/structures.ts
+++
@@ -1,6 +0,0 @@
-import { Vector3 } from "three";
-
-export interface StateVectors {
-    velocity: Vector3,
-    position: Vector3,
-}
